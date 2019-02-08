d3de4318427672d50657a79ee3dbeeb1c65a585e
Sachit <sachit.shetty@gmail.com>
2019-02-08 16:42:09 -0500

    Update Items.js

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,37 +1,60 @@
+// import React, { Component } from 'react';
+// import Item from './Item';
+// import Filter from './Filter';
+
+// class Items extends Component {
+//   state = {
+//     searchTerm: '',
+//   };
+
+//   updateSearchTerm = searchTerm => {
+//     this.setState({ searchTerm });
+//   };
+
+//   render() {
+//     const { title, items, onCheckOff, onRemove } = this.props;
+//     const { searchTerm } = this.state;
+//     return (
+//       <section className="Items">
+//         <h2>
+//           {title} ({items.length})
+//         </h2>
+//         <Filter searchTerm={searchTerm} onChange={this.updateSearchTerm} />
+//         {items
+//           .filter(item =>
+//             item.value.toLowerCase().includes(searchTerm.toLowerCase()),
+//           )
+//           .map(item => (
+//             <Item
+//               key={item.id}
+//               onCheckOff={() => onCheckOff(item)}
+//               onRemove={() => onRemove(item)}
+//               item={item}
+//             />
+//           ))}
+//       </section>
+//     );
+//   }
+// }
+
+// export default Items;
+
+
+
 import React, { Component } from 'react';
 import Item from './Item';
-import Filter from './Filter';
+import './Items.css';
 
 class Items extends Component {
-  state = {
-    searchTerm: '',
-  };
-
-  updateSearchTerm = searchTerm => {
-    this.setState({ searchTerm });
-  };
-
   render() {
-    const { title, items, onCheckOff, onRemove } = this.props;
-    const { searchTerm } = this.state;
+    const { title, items, total } = this.props;
     return (
       <section className="Items">
         <h2>
-          {title} ({items.length})
+          {title} ({items.length} / {total})
         </h2>
-        <Filter searchTerm={searchTerm} onChange={this.updateSearchTerm} />
-        {items
-          .filter(item =>
-            item.value.toLowerCase().includes(searchTerm.toLowerCase()),
-          )
-          .map(item => (
-            <Item
-              key={item.id}
-              onCheckOff={() => onCheckOff(item)}
-              onRemove={() => onRemove(item)}
-              item={item}
-            />
-          ))}
+        { this.props.children }
+        {items.map(item => <Item key={item.id} item={item} />)}
       </section>
     );
   }
